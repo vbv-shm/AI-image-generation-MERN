a6015c04676b83896cf513dcc086812fda5cb319
vaibhav <vaibhav.8june@gmail.com>
2023-10-28 23:28:33 +0530

    third

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       screens: {
